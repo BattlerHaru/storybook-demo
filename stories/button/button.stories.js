b3fd4c918cc5a929a1b238c5ad4fcd73a960a465
BattlerHaru <kronk.sdance@gmail.com>
2024-06-22 20:33:40 -0600

    :sparkles: add argTypes to button for sizes and styles

--- stories/button/button.stories.js
+++ stories/button/button.stories.js
@@ -2,6 +2,30 @@ import { createButton } from "./button";
 
 export default {
   title: "Design System/Atoms/Button",
+  argTypes: {
+    label: {
+      name: "label",
+      control: {
+        type: "text",
+      },
+    },
+
+    style: {
+      name: "style",
+      options: ["filled", "outline"],
+      control: {
+        type: "radio",
+      },
+    },
+
+    size: {
+      name: "style",
+      options: ["small", "medium", "large"],
+      control: {
+        type: "radio",
+      },
+    },
+  },
 };
 
 const Template = ({ label, ...args }) => {
