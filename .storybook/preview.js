e146b088cd4bf9cbbbbf1cdd788cfb82a6d34e7f
BattlerHaru <kronk.sdance@gmail.com>
2024-06-23 00:36:02 -0600

    :iphone: change a viewport on preview to example

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+const customViewPorts = {
+  littlePhone: {
+    name: "iPhone 5",
+    style: {
+      width: "360px",
+      height: "540px",
+    },
+  },
+  mediumPhone: {
+    name: "iPhone 12",
+    style: {
+      width: "1024px",
+      height: "1200px",
+    },
+  },
+};
+
 /** @type { import('@storybook/html').Preview } */
 const preview = {
   parameters: {
@@ -6,6 +23,10 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: customViewPorts,
+      defaultViewport: "someDefault",
     },
   },
 };
