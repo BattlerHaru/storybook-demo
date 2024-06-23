cdafef8d2faae77f2db3aca171503f31e21f828a
BattlerHaru <kronk.sdance@gmail.com>
2024-06-22 19:47:42 -0600

    :lipstick: add styles to button, sizes and styles

--- stories/button/button.js
+++ stories/button/button.js
@@ -1,10 +1,14 @@
 import "./button.css";
 
-export const createButton = ({ style = "outlined", size = "small", label }) => {
+export const createButton = ({ style = "filled", size = "small", label }) => {
   const button = document.createElement("button");
   button.type = "submit";
   button.role = "button";
   button.innerHTML = label;
+
+  button.className = ["button", `button--${size}`, `button--${style}`].join(
+    " "
+  );
 
   return button;
 };
